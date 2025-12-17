da999c0ad42453dd8f51e7fd8ed726b13705f42a
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-12-16 21:01:18 -0300

    Fonalizei a conexão com postgreSQL completa.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-
+const testRoutes = require("./routes/test");
 const app = express();
 
 // view engine
@@ -10,6 +10,7 @@ app.set("view engine", "ejs");
 // middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/test-db", testRoutes);
 
 // home (temporário)
 app.get("/", (req, res) => {
