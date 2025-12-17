44b9d4bcdf12fa107b0d5abfdaf4d025c38fb96a
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-12-16 22:33:10 -0300

    Preparei o projeto para o deploy.

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.get("/", (req, res) => {
 });
 
 // server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
